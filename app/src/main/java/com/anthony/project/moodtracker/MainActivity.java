e0f17e8459e797cc03910e57fbf68cc1ed661247
AnthonyLambert <mrlambertanthony@gmail.com>
2020-03-12 01:19:28 +0100

    Build main gui for application

--- app/src/main/java/com/anthony/project/moodtracker/MainActivity.java
+++ app/src/main/java/com/anthony/project/moodtracker/MainActivity.java
@@ -1,14 +1,64 @@
 package com.anthony.project.moodtracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager2 viewPager2;
+    private List<Mood> moodList;
+    private MoodAdapter moodAdapter;
+    private Button historicButton;
+    private Button addMoodButton;
+    private TextView text_view;
+    private ArrayList<Mood> moodsData;
+
+    RecyclerView recyclerView;
+
+
+    public static final int[] tableauImg = new int[]{R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
+
+    public static final int[] tableauFnd = new int[]{R.color.faded_red, R.color.warm_grey, R.color.cornflower_blue_65, R.color.light_sage, R.color.banana_yellow};
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AndroidThreeTen.init(this);
         setContentView(R.layout.activity_main);
+        viewPager2 = findViewById(R.id.viewPager2);
+        historicButton = findViewById(R.id.btnHist);
+        addMoodButton = findViewById(R.id.btnComm);
+
+
+        moodAdapter = new MoodAdapter(this, tableauImg);
+        viewPager2.setAdapter(moodAdapter);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                viewPager2.setBackgroundColor(getResources().getColor(tableauFnd[position]));
+                Log.i("test position", "onPageSelected: position" + position);
+            }
+        });
+
+
     }
+
 }
